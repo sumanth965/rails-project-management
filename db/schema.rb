2866d2dbb7ada51b3c721985a3d1c15b82803b3a
sumanth965 <sumanthpoojary965@gmail.com>
2026-05-01 21:04:47 +0530

    DASHBOARD CODE AND DESIGN UPDATED

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_01_090000) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_01_101000) do
   create_table "project_memberships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "project_id", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_01_090000) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "api_token"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,6 +61,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_01_090000) do
     t.string "reset_password_token"
     t.integer "role", default: 0, null: false
     t.datetime "updated_at", null: false
+    t.index ["api_token"], name: "index_users_on_api_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
